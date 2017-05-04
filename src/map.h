3100de9a85915466a7b3596dd06486822d9e0a08
nicolopomini <nicolo.pomini@studenti.unitn.it>
2017-05-04 13:24:10 +0200

    Update map.h

--- src/map.h
+++ src/map.h
@@ -8,7 +8,7 @@ struct node_t
 	map sx, dx;
 };
 void map_init(map *m);
-void map_add(map *m, char *name, int pid); 	//se già esistente sovrascrive
+int map_add(map *m, char *name, int pid); 	
 map map_lookup_getnode(map m, char *name);
 int map_lookup(map m, char *name);	//-1 se non trovato
 void map_delete(map *m, char *name);
