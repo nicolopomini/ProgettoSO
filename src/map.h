91cd3cdf9ba62ea1a919ad1d813417980807a566
ivan martini <ivanmartini96@gmail.com>
2017-05-09 14:33:01 +0200

    Add files via upload

--- src/map.h
+++ src/map.h
@@ -1,19 +1,20 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include "tree.h"
+
 typedef struct node_t * map;
 
 struct node_t
 {
 	char *name;
-	int pid;
+	tree* pid;
 	map sx, dx;
 };
 
 void map_init(map *m);
-int map_add(map *m, char *name, int pid);
-map map_lookup_getnode(map m, char *name);
-int map_lookup(map m, char *name);	//-1 se non trovato
+int map_add(map *m, char *name, tree* pid);  //0 se gia' presente
+tree* map_lookup(map m, char *name);	//NULL se non trovato
 void map_delete(map *m, char *name);
 
 #endif
